191a9b6da27d5a7c0e1a0b3bb7b6408098a39f56
YutaroMichika <y.hazama24@gmail.com>
2025-12-04 03:37:36 +0900

    save

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  // ★ GitHub Pages のパスに合わせる
   base: '/kokushi-app/',
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [
